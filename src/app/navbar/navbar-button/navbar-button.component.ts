a583e835b676b2f016ded92857566d9273c8d1b6
Nico Döbele <nico.doebele@googlemail.com>
2023-04-03 13:59:30 +0200

    added simple navbar

---
+++ src/app/navbar/navbar-button/navbar-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-button',
+  templateUrl: './navbar-button.component.html',
+  styleUrls: ['./navbar-button.component.css']
+})
+export class NavbarButtonComponent {
+  @Input() tab!: string;
+}
